148f7fb5ba44cce3d2880fe5812fbc75dbf0649c
unknown <daza.ronnie.09212002@gmail.com>
2022-02-10 21:22:53 +0800

    Import the module json and FPDF Commit

---
+++ My PDF Resume Creator Python.py
@@ -0,0 +1,11 @@
+# Assignment 9
+# PDF Resume Creator
+# Create a python program that will create your personal resume in PDF format.
+# All personal details are stored in a JSON file.
+# Your program should read the JSON file and write the details in the PDF.
+# The output should be: LASTNAME_FIRSTNAME.pdf
+
+# Steps
+# 1. Import the module json and FPDF.
+import json
+from fpdf import FPDF
